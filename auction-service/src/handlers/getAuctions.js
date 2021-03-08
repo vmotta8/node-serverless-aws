8ef168be20ea076c699607953c32fce36084f556
Vinicius Motta <viniciusmotta8@gmail.com>
2021-03-08 11:30:21 -0300

    feat: create get auctions

---
+++ auction-service/src/handlers/getAuctions.js
@@ -0,0 +1,33 @@
+import AWS from 'aws-sdk'
+import middy from '@middy/core'
+import httpJsonBodyParser from '@middy/http-json-body-parser'
+import httpEventNormalizer from '@middy/http-event-normalizer'
+import httpErrorHandler from '@middy/http-error-handler'
+import createError from 'http-errors'
+
+const dynamodb = new AWS.DynamoDB.DocumentClient()
+
+async function getAuctions(event, context) {
+  let auctions
+
+  try {
+    const result = await dynamodb.scan({ 
+      TableName: process.env.AUCTIONS_TABLE_NAME
+    }).promise()
+
+    auctions = result.Items
+  } catch (err) {
+    console.log(err)
+    throw new createError.InternalServerError(err)
+  }
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({ auctions }),
+  };
+}
+
+export const handler = middy(getAuctions)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler())
