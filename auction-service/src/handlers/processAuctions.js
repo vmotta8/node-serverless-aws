155f9ebe24798d486ecdc762d2c8766f900e585f
Vinicius Motta <viniciusmotta8@gmail.com>
2021-03-09 11:53:41 -0300

    feat: create close auction function

--- auction-service/src/handlers/processAuctions.js
+++ auction-service/src/handlers/processAuctions.js
@@ -1,8 +1,17 @@
+import createError from 'http-errors'
 import { getEndedAuction } from '../lib/getEndedAuctions'
+import { closeAuction } from '../lib/closeAuction'
 
 async function processAuctions(event, context) {
-  const auctionsToClose = await getEndedAuction()
-  console.log(auctionsToClose)
+  try {
+    const auctionsToClose = await getEndedAuction()
+    const closePromises = auctionsToClose.map(auction => closeAuction(auction))
+    await Promise.all(closePromises)
+    return { closed: closePromises.length }
+  } catch (err) {
+    console.log(err)
+    throw new createError.InternalServerError(err)
+  }
 }
 
 export const handler = processAuctions
