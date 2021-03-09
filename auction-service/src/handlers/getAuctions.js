77721f4f621b498e977199946f74bd53d42d82b7
Vinicius Motta <viniciusmotta8@gmail.com>
2021-03-09 15:58:34 -0300

    feat: add query string parameters schema

--- auction-service/src/handlers/getAuctions.js
+++ auction-service/src/handlers/getAuctions.js
@@ -1,6 +1,8 @@
 import AWS from 'aws-sdk'
 import commonMiddleware from '../lib/commonMiddleware'
 import createError from 'http-errors'
+import validator from '@middy/validator'
+import getAuctionsSchema from '../lib/schemas/getAuctionsSchema'
 
 const dynamodb = new AWS.DynamoDB.DocumentClient()
 
@@ -36,3 +38,4 @@ async function getAuctions(event, context) {
 }
 
 export const handler = commonMiddleware(getAuctions)
+  .use(validator({ inputSchema: getAuctionsSchema, useDefaults: true }))
