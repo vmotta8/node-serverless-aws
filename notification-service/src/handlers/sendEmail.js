b1edae8686bdf6064174dfc42d1856dd194f419d
Vinicius Motta <viniciusmotta8@gmail.com>
2021-03-10 12:31:53 -0300

    feat: add aws sqs

--- notification-service/src/handlers/sendEmail.js
+++ notification-service/src/handlers/sendEmail.js
@@ -3,19 +3,25 @@ import AWS from 'aws-sdk'
 const ses = new AWS.SES({ region: 'us-east-1' })
 
 async function sendMail(event, context) {
+  const record = event.Records[0]
+  console.log('record processing', record)
+
+  const email = JSON.parse(record.body)
+  const { subject, body, recipient } = email
+
   const params = {
     Source: 'viniciusmotta8@gmail.com',
     Destination: {
-      ToAddresses: ['viniciusmotta8@gmail.com'],
+      ToAddresses: [recipient],
     },
     Message: {
       Body: {
         Text: {
-          Data: 'Hello!',
+          Data: body,
         },
       },
       Subject: {
-        Data: 'Test Mail',
+        Data: subject,
       },
     },
   }
