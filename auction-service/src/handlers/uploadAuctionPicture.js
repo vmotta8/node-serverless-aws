a6da3f0d21c8146a3269d79e543c0e154280c690
Vinicius Motta <viniciusmotta8@gmail.com>
2021-03-10 19:49:49 -0300

    feat: add validations

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -1,14 +1,22 @@
 import { getAuctionById } from './getAuction'
 import { uploadPictureToS3 } from '../lib/uploadPictureToS3'
 import middy from '@middy/core'
+import validator from '@middy/validator'
 import httpErrorHandler from '@middy/http-error-handler'
 import createError from 'http-errors'
 import { setAuctionPictureUrl } from '../lib/setAuctionPictureUrl'
+import uploadAuctionPictureSchema from '../lib/schemas/uploadAuctionPictureSchema'
 
 
 export async function uploadAuctionPicture(event) {
+  const { email } = event.requestContext.authorizer;
   const { id } = event.pathParameters
   const auction = await getAuctionById(id)
+
+  if (email !== auction.seller) {
+    throw new createError.Forbidden(`You are not the seller of this auction.`)
+  }
+
   const base64 = event.body.replace(/^data:image\/\w+base64,/, '')
   const buffer = Buffer.from(base64, 'base64')
 
@@ -29,3 +37,4 @@ export async function uploadAuctionPicture(event) {
 
 export const handler = middy(uploadAuctionPicture)
   .use(httpErrorHandler())
+  .use(validator({ inputSchema: uploadAuctionPictureSchema}))
